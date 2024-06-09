1d7495f584eab693154f2e961e7f4af87ca98e0e
ElMatiP03 <162301823+ElMatiP03@users.noreply.github.com>
2024-06-09 19:43:20 +0200

    Add files via upload

---
+++ src/constants/educacionDataEs.js
@@ -0,0 +1,51 @@
+import imagenes from "./images";
+
+const EducacionDataEs = [
+  {
+    img: imagenes.unlam,
+    carrera: "Técnico en desarrollo de aplicaciones web y móviles",
+    institucion: "Universidad Nacional de La Matanza",
+    estado: "Completado",
+    fecha: "Marzo 2017 - Diciembre 2020",
+    descargar: "",
+    tipo: "Universitario",
+  },
+  {
+    img: imagenes.educacionIT,
+    carrera: "FullStack Developer",
+    institucion: "Educacion IT",
+    estado: "Completado",
+    fecha: "Marzo 2016 - Diciembre 2017",
+    descargar: "",
+    tipo: "Curso",
+  },
+  {
+    img: imagenes.educacionIT,
+    carrera: "Seguridad informática",
+    institucion: "Educacion IT",
+    estado: "Completado",
+    fecha: "Marzo 2014 - Diciembre 2016",
+    descargar: "",
+    tipo: "Curso",
+  },
+  {
+    img: imagenes.jorgeNewbery,
+    carrera: "Técnico aeronautico",
+    institucion: "EET Nº8 Jorge Newbery",
+    estado: "Completado",
+    fecha: "Marzo 2011 - Diciembre 2012",
+    descargar: "",
+    tipo: "Terciario técnico",
+  },
+  {
+    img: imagenes.jorgeNewbery,
+    carrera: "Mecánico en mantenimiento de aeronaves",
+    institucion: "EET Nº8 Jorge Newbery",
+    estado: "Completado",
+    fecha: "Marzo 2007 - Diciembre 2011",
+    descargar: "",
+    tipo: "Secundario",
+  },
+];
+
+export default EducacionDataEs;
---
+++ src/constants/laboralDataEs.js
@@ -0,0 +1,132 @@
+import imagenes from "./images";
+
+const LaboralDataEs = [
+  {
+    id: "selehann",
+    puesto: "Desarrollador Full Stack",
+    empresa: "Selehann",
+    client: " iDocket",
+    desde: "Abril 2022",
+    hasta: "Actualidad",
+    tareas:
+      "Mantenimiento del aplicación en ReactJS, incluyendo bibliotecas como Hooks/Proveedores de Contexto, Redux, React Router, y bibliotecas de CSS/Sass como Bootstrap y MaterialUI. Y trabajando con el uso de arquitecturas y desarrollo de microservicios con .NET/.NET Core, utilizando inyección de dependencias, servicios wcf y servicios en la nube de Amazon, manejando datos con MySQL y Entity Framework.",
+    techSkills: [
+      ".Net 6",
+      ".Net Core 3.1",
+      "ReactJS",
+      "ASP Web/Win forms",
+      "MySQL",
+      "AWS",
+    ],
+    logo: imagenes.selehann,
+  },
+  {
+    id: "theBridge",
+    puesto: "Full Stack Developer",
+    empresa: "The Bridge",
+    client: "Grace Kennedy Financial Group",
+    desde: "Febrero 2021",
+    hasta: "Abril 2022",
+    tareas:
+      "Diseño y desarrollo de APIs, Identificación y análisis de los requisitos de usuario para crear diferentes endpoints para el equipo de Front-end. Prioritización, asignación y creación de tareas en todo el ciclo de vida del desarrollo de software.",
+    techSkills: [
+      ".Net 6",
+      ".Net Core 3.1",
+      " ReactJS",
+      " NodeJS",
+      "Azure pipeline",
+      "Azure Cosmos DB",
+      "Azure scheduler Service",
+      "Mailer service",
+    ],
+    logo: imagenes.theBridge,
+  },
+  {
+    id: "globant",
+    puesto: "Lider técnico y Desarrollador Full Stack",
+    empresa: "Urbetrack",
+    desde: "Abril 2019",
+    hasta: "Febrero 2021",
+    tareas:
+      "Desarrollo de nuevas funciones en MVC (vistas Razor), Restful WebAPIs, base de datos SQL, pruebas unitarias usando NUnit y MOQ, servicios de datos TCP. Responsable de identificar áreas de una aplicación web que están causando problemas de rendimiento e implementar optimizaciones para mejorar la velocidad y escalabilidad general de la aplicación.",
+    techSkills: [
+      ".Net",
+      ".Net Core",
+      "SQL Database",
+      "APIs",
+      "Microservices wcf/wsdl",
+    ],
+    logo: imagenes.urbetrack,
+  },
+  {
+    id: "globant",
+    puesto: "Desarrollador Full Stack",
+    empresa: "Globant",
+    client: "JPMogan / EY",
+    desde: "Septiembre 2017",
+    hasta: "Abril 2019",
+    tareas:
+      "Desarrollo de nuevas funciones en MVC (vistas en Razor), Restful WebAPIs, base de datos SQL, pruebas unitarias usando NUnit y MOQ, microservicios TCP/SOAP. Responsable de identificar áreas de la aplicación web que están causando problemas de rendimiento e implementar optimizaciones para mejorar la velocidad y escalabilidad general.",
+    techSkills: [
+      ".Net",
+      ".Net Core",
+      "SQL Database",
+      "APIs",
+      "Microservices wcf/wsdl",
+    ],
+    logo: imagenes.globant,
+  },
+  {
+    id: "justSolution",
+    puesto: "Desarrollador .Net",
+    empresa: "Just Solution",
+    desde: "Noviembre 2016",
+    hasta: "Septiembre 2017",
+    tareas:
+      "Desarrollo Full Stack de plataformas web en WebForms, ASP/ASPX, MVC y bases de datos SQL con ORM personalizado.",
+    techSkills: [
+      "Visual Basic",
+      ".Net",
+      "WinForm",
+      "WebForms / MVC / SQL Server",
+      "Mongo DB",
+    ],
+    logo: imagenes.justSolution,
+  },
+  {
+    id: "formanova",
+    puesto: "Representante de Ventas Ejecutivo",
+    empresa: "Formanova",
+    desde: "Febrero 2015",
+    hasta: "Noviembre 2016",
+    tareas:
+      "Ejecutivo de ventas para distribuidores, importador de productos de China y vendedor de productos directo de fábrica",
+    techSkills: [],
+    logo: imagenes.formanova,
+  },
+  {
+    id: "army",
+    puesto: "Soldado técnico especialista en Aeronaves",
+    empresa:
+      "Ejército Argentino, Batallón de abastecimiento y mantenimiento 601",
+    desde: "Enero 2014",
+    hasta: "Febrero 2015",
+    tareas:
+      "Mantenimiento preventivo para helicópteros y aeronaves del batallón, manejo de piezas aeronáuticas, categorización de su estado y reemplazo.",
+    techSkills: [],
+    logo: imagenes.army,
+  },
+  {
+    id: "ncr",
+    puesto: "Mesa de ayuda",
+    empresa: "NCR",
+    desde: "Enero 2012",
+    hasta: "Enero 2014",
+    tareas:
+      "Soporte técnico de nivel 2, respondiendo a consultas de clientes, solucionando problemas y proporcionando soluciones a través de teléfono, correo electrónico o chat.",
+    techSkills: [],
+    logo: imagenes.ncr,
+  },
+];
+
+export default LaboralDataEs;
